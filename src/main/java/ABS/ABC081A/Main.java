d9b01bb362ad0c057aad9d42a1d0bd92a49e30cd
mutohiroshi <mutohiroshi@team-lab.com>
2019-12-22 15:42:47 +0900

    ABC081A

---
+++ src/main/java/ABS/ABC081A/Main.java
@@ -0,0 +1,20 @@
+package ABC081A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '1') {
+                count++;
+            }
+        }
+        System.out.println(count);
+        sc.close();
+    }
+}
