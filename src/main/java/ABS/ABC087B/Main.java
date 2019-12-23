c87663e7ae2301f2afbdc2ef99c5f0882b8d5863
mutohiroshi <mutohiroshi@team-lab.com>
2019-12-24 00:09:00 +0900

    ABC087B

---
+++ src/main/java/ABS/ABC087B/Main.java
@@ -0,0 +1,25 @@
+package ABC087B;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int x = sc.nextInt();
+        
+        int count = 0;
+        for(int An=0; An<=a; An++){
+            for(int Bn=0; Bn<=b; Bn++){
+                for(int Cn=0; Cn<=c; Cn++){
+                    if((An*500+Bn*100+Cn*50)==x){
+                        count++;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
