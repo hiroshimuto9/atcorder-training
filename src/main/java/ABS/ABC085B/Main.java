6deae211a2df5d18cede395e1e2d7810ef8c4b83
mutohiroshi <mutohiroshi@team-lab.com>
2019-12-25 13:26:09 +0900

    ABC085B

---
+++ src/main/java/ABS/ABC085B/Main.java
@@ -0,0 +1,29 @@
+package ABC085B;
+
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer> dList = new ArrayList<>();
+        for (int i=0; i < n; i++) {
+            int d = sc.nextInt();
+            dList.add(d);
+        }
+        sc.close();
+    
+        /*
+            重複を取り除いた数値のSetを作成
+         */
+        Set<Integer> checkHash = new HashSet<>();
+        for (Integer d : dList) {
+            if (!checkHash.contains(d)) {
+                checkHash.add(d);
+            }
+        }
+        
+        System.out.println(checkHash.size());
+    }
+}
