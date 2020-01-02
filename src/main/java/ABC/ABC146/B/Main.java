101dea4dfe2d2411631cc3740b096e065d01e837
mutohiroshi <mutohiroshi@team-lab.com>
2020-01-02 18:03:04 +0900

    ABC146 B

--- src/main/java/ABC/ABC146/B/Main.java
+++ src/main/java/ABC/ABC146/B/Main.java
@@ -1,7 +1,20 @@
 package ABC.ABC146.B;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        char[] S = sc.next().toCharArray();
+        int length = S.length;
     
+        // Asciiコードの性質を用いて文字列を直接計算して変えていく。
+        for (int i = 0; i < length; i++) {
+            int n = (S[i] - 'A' + N) % 26;
+            S[i] = (char) (n + 'A');
+        }
+    
+        System.out.println((new String(S)));
     }
 }
