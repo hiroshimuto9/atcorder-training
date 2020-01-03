f2e6a75b424708f41966ee1f3392e27e154d021f
mutohiroshi <mutohiroshi@team-lab.com>
2020-01-03 20:55:05 +0900

    ABC145 B

--- src/main/java/ABC/ABC145/B/Main.java
+++ src/main/java/ABC/ABC145/B/Main.java
@@ -1,5 +1,24 @@
 package ABC.ABC145.B;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        String S = sc.next();
+        
+        if (N % 2 == 1) {
+            System.out.println("No");
+        } else {
+            String t1 = S.substring(0, N / 2);
+            String t2 = S.substring(N / 2, N);
+            
+            if (t1.equals(t2)) {
+                System.out.println("Yes");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
 }
