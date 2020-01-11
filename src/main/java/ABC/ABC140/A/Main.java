55e7ae3cfc6ca807bcdbed0c10d14fd62968d543
mutohiroshi <mutohiroshi@team-lab.com>
2020-01-11 18:10:44 +0900

    ABC140 A

---
+++ src/main/java/ABC/ABC140/A/Main.java
@@ -0,0 +1,13 @@
+package ABC.ABC140.A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int result = (int)Math.pow(N,3);
+        System.out.println(result);
+    }
+}
