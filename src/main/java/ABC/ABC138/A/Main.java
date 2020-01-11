ce8af66c559d3bd0e3d57d2d406903ae4e5f7602
mutohiroshi <mutohiroshi@team-lab.com>
2020-01-11 23:44:29 +0900

    ABC138 A

---
+++ src/main/java/ABC/ABC138/A/Main.java
@@ -0,0 +1,17 @@
+package ABC.ABC138.A;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String s = sc.next();
+        
+        if (a < 3200) {
+            System.out.println("red");
+        } else {
+            System.out.println(s);
+        }
+    }
+}
