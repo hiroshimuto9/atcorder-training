f28c57de024c5f32334882788bde0f4bbfb210d7
mutohiroshi <mutohiroshi@team-lab.com>
2020-01-13 00:34:38 +0900

    ABC136 B

---
+++ src/main/java/ABC/ABC136/B/Main.java
@@ -0,0 +1,18 @@
+package ABC.ABC136.B;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        
+        int answer = 0;
+        for (int i=1; i<=N; i++) {
+            if (String.valueOf(i).length() % 2 == 1) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+    }
+}
